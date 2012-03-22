fc50b8d3060be29cae96a85116d6ddc545b5e66a
aereal <aereal@kerare.org>
2012-03-22 12:32:42 +0900

    Use Class::Accessor::Fast

--- lib/My/List.pm
+++ lib/My/List.pm
@@ -2,22 +2,16 @@ package My::List;
 use strict;
 use warnings;
 
+use base 'Class::Accessor::Fast';
+
+__PACKAGE__->mk_accessors(qw/head tail/);
+
 sub new {
-    my $class = shift;
-    my ($head, @rest) = @_;
-    my $self = {head => $head, tail => @rest == 0 ? undef : $class->new(@rest)};
-    bless $self, $class;
-    return $self;
-}
-
-sub head {
-    my $self = shift;
-    $self->{head};
-}
-
-sub tail {
-    my $self = shift;
-    $self->{tail};
+    my ($class, $head, @rest) = @_;
+    $class->SUPER::new({
+        head => $head,
+        tail => @rest == 0 ? undef : $class->new(@rest)
+    });
 }
 
 1;
